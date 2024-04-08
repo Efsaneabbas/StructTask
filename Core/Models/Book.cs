557a6742bb54e1ef0b37c90e12cbce4c77bebcf9
Efsaneabbas <96765288+Efsaneabbas@users.noreply.github.com>
2024-04-08 18:59:18 +0400

    task added

---
+++ Core/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    internal class Book
+    {
+        public static int _no;
+        public int No { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+         
+
+        public Book()
+        {
+
+        }
+
+        public Book(string name,  string authorName, int pageCount)
+        {
+            _no++;
+            No = _no ;
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+
+        }
+
+    }
+}
---
+++ Core/Models/Library.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    internal class Library
+    {
+        public Library() 
+        {
+
+        }
+
+        Book[] Books = new Book[] { };
+        public void Book (Book book)
+        {
+
+            Array.Resize(ref Books, Books.Length+1);
+            Books[Books.Length-1] = book;
+        }
+        public void FindAllBooksByName(string name)
+        {
+            Book[] Books = { };
+            foreach (Book book in Books)
+            {
+                if (book.Name == name)
+                {
+                    Array.Resize(ref Books, Books.Length+1);
+                    Books[Books.Length-1]= book;
+                }
+            } return Book;
+        }
+
+        public void RemoveAllBooks(string Name )
+        {
+            Book[] Books = { };
+            foreach (Book book in Books)
+            {
+                Array.Resize(ref Books, Books.Length + 1);
+                Books[Books.Length - 1] = book;
+            }
+            Books = Books;
+        }
+        public void SeachBook(string Name, int No, string AuthorName, int PageCount)
+        {
+
+            Book[] Books = { };
+
+
+
+        }
+    }
+
+ 
+
+
+}
